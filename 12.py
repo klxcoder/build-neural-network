8bb4415c4f06f62394fb76fefe3a888862a0d6c5
klxcoder <klxcoder@gmail.com>
2025-03-22 22:46:37 +0700

    Layer_Dense

---
+++ 12.py
@@ -0,0 +1,15 @@
+from typing import List
+
+import numpy as np
+
+X: List[List[float]] = [
+    [1, 2, 3, 2.5],
+    [2.0, 5.0, -1.0, 2.0],
+    [-1.5, 2.7, 3.3, -0.8],
+]
+
+class Layer_Dense:
+    def __init__(self):
+        pass
+    def forward(self):
+        pass
