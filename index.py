89ccd1c5c33e97d536fb758f9ec84cacde03e75a
klxcoder <klxcoder@gmail.com>
2025-03-22 17:28:40 +0700

    versions

--- index.py
+++ index.py
@@ -1,6 +1,12 @@
+import sys
 import numpy as np
 import pandas as pd
+import matplotlib
 from matplotlib import pyplot as plt
+
+print('Python:', sys.version) # Python: 3.10.16 (main, Dec 11 2024, 16:24:50) [GCC 11.2.0]
+print('Numpy:', np.__version__) # Numpy: 2.2.4
+print('Matplotlib:', matplotlib.__version__) # Matplotlib: 3.10.1
 
 # Test NumPy
 print("NumPy Test:", np.array([1, 2, 3]) + 1)
