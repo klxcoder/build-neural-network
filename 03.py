0cfb40609ccfcaf304693a236b9c8bb0e35b5421
klxcoder <klxcoder@gmail.com>
2025-03-22 18:29:05 +0700

    poetry run python 03.py

--- 03.py
+++ 03.py
@@ -1,12 +1,12 @@
 from typing import List
 
-inputs: List[float] = [1, 2, 3]
-weights: List[float] = [0.2, 0.8, -0.5]
+inputs: List[float] = [1, 2, 3, 2.5]
+weights: List[float] = [0.2, 0.8, -0.5, 1.0]
 bias: float = 2
 
-sum: float = inputs[0] * weights [0] + inputs[1] * weights [1] + inputs[2] * weights [2]
+sum: float = inputs[0] * weights [0] + inputs[1] * weights [1] + inputs[2] * weights [2] + inputs[3] * weights [3]
 
 output: float = sum + bias
 
-print(sum) # 0.30000000000000004
-print(output) # 2.3
+print(sum) # 2.8
+print(output) # 4.8
