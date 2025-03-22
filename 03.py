1017af3119cba048557cb948c88264bf0ddbf35c
klxcoder <klxcoder@gmail.com>
2025-03-22 18:36:50 +0700

    array of output

--- 03.py
+++ 03.py
@@ -9,6 +9,10 @@ bias1: float = 2
 bias2: float = 3
 bias3: float = 0.5
 
-output: float = inputs[0] * weights1 [0] + inputs[1] * weights1 [1] + inputs[2] * weights1 [2] + inputs[3] * weights1 [3] + bias1
+output: List[float] = [
+    inputs[0] * weights1 [0] + inputs[1] * weights1 [1] + inputs[2] * weights1 [2] + inputs[3] * weights1 [3] + bias1,
+    inputs[0] * weights2 [0] + inputs[1] * weights2 [1] + inputs[2] * weights2 [2] + inputs[3] * weights2 [3] + bias2,
+    inputs[0] * weights3 [0] + inputs[1] * weights3 [1] + inputs[2] * weights3 [2] + inputs[3] * weights3 [3] + bias3,
+]
 
-print(output) # 4.8
+print(output) # [4.8, 1.21, 2.385]
