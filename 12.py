fc5c496b9de975e4f935a39f18c56e414c3d1235
klxcoder <klxcoder@gmail.com>
2025-03-22 22:53:27 +0700

    np.random

--- 12.py
+++ 12.py
@@ -2,6 +2,8 @@ from typing import List
 
 import numpy as np
 
+np.random.seed(0)
+
 X: List[List[float]] = [
     [1, 2, 3, 2.5],
     [2.0, 5.0, -1.0, 2.0],
@@ -9,7 +11,15 @@ X: List[List[float]] = [
 ]
 
 class Layer_Dense:
-    def __init__(self):
-        pass
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = np.random.randn(n_inputs, n_neurons)
     def forward(self):
         pass
+
+print(np.random.randn(4, 3))
+"""
+[[ 1.76405235  0.40015721  0.97873798]
+ [ 2.2408932   1.86755799 -0.97727788]
+ [ 0.95008842 -0.15135721 -0.10321885]
+ [ 0.4105985   0.14404357  1.45427351]]
+"""
