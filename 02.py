a841086e4252062858a929ec420b6c7129307f4f
klxcoder <klxcoder@gmail.com>
2025-03-22 17:56:24 +0700

    typing for 02.py

--- 02.py
+++ 02.py
@@ -1,10 +1,12 @@
-inputs = [1.2, 5.1, 2.1]
-weights = [3.1, 2.1, 8.7]
-bias = 3
+from typing import List
 
-sum = inputs[0] * weights [0] + inputs[1] * weights [1] + inputs[2] * weights [2]
+inputs: List[float] = [1.2, 5.1, 2.1]
+weights: List[float] = [3.1, 2.1, 8.7]
+bias: float = 3
 
-output = sum + bias
+sum: float = inputs[0] * weights [0] + inputs[1] * weights [1] + inputs[2] * weights [2]
+
+output: float = sum + bias
 
 print(sum) # 32.7
 print(output) # 35.7
