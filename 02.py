28155a72bf17b6f7f7286dd3ad9de71be492d007
klxcoder <klxcoder@gmail.com>
2025-03-22 17:52:03 +0700

    poetry run python 02.py

---
+++ 02.py
@@ -0,0 +1,10 @@
+inputs = [1.2, 5.1, 2.1]
+weights = [3.1, 2.1, 8.7]
+bias = 3
+
+sum = inputs[0] * weights [0] + inputs[1] * weights [1] + inputs[2] * weights [2]
+
+output = sum + bias
+
+print(sum) # 32.7
+print(output) # 35.7
