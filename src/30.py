244db3dd8f31872f4cb03f4de1ed841792e08bac
klxcoder <klxcoder@gmail.com>
2025-03-23 08:43:41 +0700

    poetry run python src/30.py

---
+++ src/30.py
@@ -0,0 +1,11 @@
+import math
+
+print(math.log(math.e)) # 1.0
+
+# print(math.log(0)) # ValueError: math domain error
+
+"""
+math.log(x)
+    if x>0 => No error
+    if x<=0 => ValueError: math domain error
+"""
