5397ceb868691eb720a21433fe73cc2ad2f82ef6
klxcoder <klxcoder@gmail.com>
2025-03-23 08:06:31 +0700

    poetry run python src/28.py

--- src/28.py
+++ src/28.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 b = 5.2
 
@@ -6,7 +7,7 @@ x = np.log(b)
 
 print(x) # 1.6486586255873816
 print(np.exp(x)) # 5.199999999999999
-
+print(math.e ** x) # 5.199999999999999
 """
 e ** x = b
 """
